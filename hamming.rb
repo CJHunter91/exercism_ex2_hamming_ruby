148637d4cb5de99af29f628a65acbab511a18441
Chris Hunter <orkem2@gmail.com>
2017-07-03 10:07:44 +0100

    Passed complete_distance_in_single_nucleotide_strands

--- hamming.rb
+++ hamming.rb
@@ -2,5 +2,18 @@ class Hamming
 
   def self.compute(strand1, strand2)
     return 0 if strand1 == strand2
+    #change to array
+    strand1_arr = strand1.chars()
+    strand2_arr = strand2.chars()
+    count = 0
+    nucleotide_not_match = 0
+    #compare each nucleotide in both arrays
+    for nucleotide in strand1_arr
+      nucleotide_not_match += 1 if nucleotide != strand2_arr[count]
+      # use count to iterate and compare relative nucleotides
+      count += 1
+    end
+
+    return nucleotide_not_match
   end
 end
