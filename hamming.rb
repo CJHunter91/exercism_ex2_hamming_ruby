d9c1a6f93418a408162f3f2650e1440cd0deaa88
Chris Hunter <orkem2@gmail.com>
2017-07-03 10:45:24 +0100

    Final Commit

--- hamming.rb
+++ hamming.rb
@@ -5,14 +5,12 @@ class Hamming
     return 0 if strand1 == strand2
     return raise(ArgumentError) if strand1.size() != strand2.size()
     #change to array
-    strand1_arr = strand1.chars()
-    strand2_arr = strand2.chars()
     count = 0
     nucleotide_not_match = 0
     #compare each nucleotide in both arrays
-    for nucleotide in strand1_arr
-      nucleotide_not_match += 1 if nucleotide != strand2_arr[count]
-      # use count to iterate and compare relative nucleotides
+    for nucleotide in strand1.chars()
+      nucleotide_not_match += 1 if nucleotide != strand2.chars()[count]
+      # use count to iterate and compare relative nucleotides  in each strand
       count += 1
     end
 
